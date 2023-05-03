bd16e7f633fa1fa9ae37d5369bebed09dade45cc
Marton Soltesz-Nagy <v-mnagy@expediagroup.com>
2023-05-03 15:50:45 +0200

    initialize netflify build

--- app/utils/cookie.ts
+++
@@ -1,9 +0,0 @@
-import { createCookie } from "@remix-run/node";
-
-export const cookie = createCookie("galactusCredentials", {
-    maxAge: 36_000,
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    path: '/'
-});
--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,18 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.ts"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
 };
